94b204392ddcd05cf27f9fcc83c47c2f7cf27d52
irinshelly <irin1021@gmail.com>
2023-06-26 08:09:27 +0300

    movie cards

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,6 +1,6 @@
-import {useEffect} from "react";
-import {useDispatch, useSelector} from "react-redux";
-import {Card, CardContent,CardMedia, Typography, Chip} from "@mui/material";
+import {useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {Card, Badge} from 'react-bootstrap';
 
 import {getMovies} from "../../services";
 import {moviesActions,moviesSelectors} from "../../store";
@@ -17,25 +17,27 @@ const Home = () => {
             console.log({data})
             dispatch(moviesActions.setMovies(data));
         });
-    }, [movies])
+    }, [movies, dispatch])
 
     return (
-        <div className="grid-layout">
+        <div className="movies-layout">
             {movies?.map(({ id, name , posterLink, length, releaseYear}) => (
                 <Card key={id}>
-                    <CardMedia
-                        component="img"
-                        height="140"
-                        image={posterLink}
+                    <Card.Img
+                        variant="top"
+                        src={posterLink}
                         alt={name}
                     />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
+                    <Card.Body>
+                        <Card.Title>
                             {name}
-                        </Typography>
-                        {!!length && <Chip label={`length: ${length}`}/>}
-                        {!!releaseYear && <Chip label={`releaseYear: ${releaseYear}`} color="primary"/>}
-                    </CardContent>
+                        </Card.Title>
+                        <div className="card-pills">
+                            {!!length && <Badge pill bg="info">length: {length}</Badge>}
+                            {!!releaseYear && <Badge pill bg="dark">releaseYear: {releaseYear}</Badge>}
+                        </div>
+
+                    </Card.Body>
                 </Card>
             ))}
         </div>
