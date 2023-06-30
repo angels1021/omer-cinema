3d84bdcb0d887bc1f60e8febf73de4c12ffad248
irinshelly <irin1021@gmail.com>
2023-06-30 23:22:05 +0300

    trailer

---
+++ src/components/Home/MovieCard.tsx
@@ -0,0 +1,38 @@
+import {Badge, Card, Button} from "react-bootstrap";
+import { BsYoutube } from 'react-icons/bs';
+
+import {Movie} from "../../models";
+
+const MovieCard = ({ id, name , posterLink, length, releaseYear, videoLink }: Movie) => {
+
+    return (
+        <Card key={id}>
+            <Card.Img
+                variant="top"
+                src={posterLink}
+                alt={name}
+            />
+            <Card.Body>
+                <Card.Title className="d-flex gap-2 flex-wrap">
+                    {name}
+                </Card.Title>
+                <div className="card-pills mb-3">
+                    {!!length && <Badge pill bg="info">length: {length}min</Badge>}
+                    {!!releaseYear && <Badge pill bg="dark">released: {releaseYear}</Badge>}
+                </div>
+                {videoLink && (
+                    <Card.Link
+                        href={videoLink}
+                        title="watch trailer"
+                        target="_blank"
+                        className="link-secondary"
+                    >
+                        <BsYoutube className="link-danger" /> watch trailer
+                    </Card.Link>
+                )}
+                <Button variant="primary">Order tickets</Button>
+            </Card.Body>
+        </Card>
+)};
+
+export default MovieCard;
--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,9 +1,9 @@
 import {useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
-import {Card, Badge} from 'react-bootstrap';
 
 import {getMovies} from "../../services";
 import {moviesActions,moviesSelectors} from "../../store";
+import MovieCard from './MovieCard';
 import './style.css';
 
 const Home = () => {
@@ -20,25 +20,7 @@ const Home = () => {
 
     return (
         <div className="movies-layout">
-            {movies?.map(({ id, name , posterLink, length, releaseYear}) => (
-                <Card key={id}>
-                    <Card.Img
-                        variant="top"
-                        src={posterLink}
-                        alt={name}
-                    />
-                    <Card.Body>
-                        <Card.Title>
-                            {name}
-                        </Card.Title>
-                        <div className="card-pills">
-                            {!!length && <Badge pill bg="info">length: {length}</Badge>}
-                            {!!releaseYear && <Badge pill bg="dark">releaseYear: {releaseYear}</Badge>}
-                        </div>
-
-                    </Card.Body>
-                </Card>
-            ))}
+            {movies?.map(movie => <MovieCard key={movie.id} {...movie} />)}
         </div>
     )
 };
