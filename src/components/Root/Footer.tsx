387c2b6406a804a42067b1b000e28b3b1051003b
irinshelly <irin1021@gmail.com>
2023-07-02 17:20:29 +0300

    style

--- src/components/Root/Footer.tsx
+++ src/components/Root/Footer.tsx
@@ -2,7 +2,9 @@ import ThemeSwitch from "./ThemeSwitch";
 
 const Footer = () => (
     <footer className="d-flex justify-content-between p-3">
-        <div>dev info</div>
+        <div>
+            Developed by <a href="mailto:efisabo@hotmail.com">Efi Sabo</a>
+        </div>
         <ThemeSwitch />
     </footer>
 );
